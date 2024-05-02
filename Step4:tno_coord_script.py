7160f343a9f64152aaf31e65c3a0a506931de214
Simran Kamboj <72260678+SimranKKamboj@users.noreply.github.com>
2024-05-02 16:25:08 -0700

    Update Step4:tno_coord_script.py

--- Step4:tno_coord_script.py
+++ Step4:tno_coord_script.py
@@ -16,6 +16,6 @@ for line in lines:
         columns.append(first_two_columns)
 
 # Write the first two columns from each line to tno.txt
-with open('tno.txt', 'w') as output_file:
+with open('tno_coord.txt', 'w') as output_file:
     for column_pair in columns:
         output_file.write("\t".join(column_pair) + "\n")  # Separate the columns with a tab and add a new line
